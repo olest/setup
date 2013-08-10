27128c0f3d52466ac5412957f3c7489258333610
Ole Schulz-Trieglaff <ole.st@gmx.de>
2013-08-10 19:56:17 +0100

    Adding octave and R to apt-get install list

--- setup.kaggle.sh
+++ setup.kaggle.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
-# for headless setup. 
+# installs typical tools needed for kaggle competitions
+
+sudo apt-get update
 
 sudo apt-get install g++
+sudo apt-get install octave
 
 # python essentials
 sudo apt-get install python
@@ -10,13 +13,13 @@ sudo apt-get install python-scipy
 sudo apt-get install python-numpy
 
 # R stuff
-sudo apt-get install R
+sudo apt-get install r-base-core
 wget http://cran.r-project.org/src/contrib/randomForest_4.6-7.tar.gz
 R CMD INSTALL randomForest_4.6-7.tar.gz
 
 # cause effect data and git repo
 #wget http://mlg.eng.cam.ac.uk/lloyd/temp/raw.tar.gz
-git clone git@github.com:jamesrobertlloyd/cause-effect.git
+git clone https://github.com/jamesrobertlloyd/cause-effect.git
 
 # git pull and install dotfiles as well
 cd $HOME
@@ -26,10 +29,11 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+
+#git clone https://github.com/startup-class/dotfiles.git
+#ln -sb dotfiles/.screenrc .
+#ln -sb dotfiles/.bash_profile .
+#ln -sb dotfiles/.bashrc .
+#ln -sb dotfiles/.bashrc_custom .
+#ln -sf dotfiles/.emacs.d .
 
